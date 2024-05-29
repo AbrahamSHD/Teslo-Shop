30ebc8260895459437a04e87c566e482dc1488a7
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-05-29 17:10:22 -0600

    FileService Created

---
+++ src/files/files.service.ts
@@ -0,0 +1,19 @@
+import { existsSync } from 'fs';
+import { join } from 'path';
+
+import { BadRequestException, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class FilesService {
+  
+  getStaticImage( imageName: string ) {
+
+    const path = join( __dirname, '../../static/products', imageName )
+
+    if ( !existsSync(path) )
+      throw new BadRequestException(`Image with name: "${ imageName }" not found`)
+    
+    return path
+  }
+
+}
