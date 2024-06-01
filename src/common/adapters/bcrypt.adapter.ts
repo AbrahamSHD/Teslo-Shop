460be8b1e8c691cb8f57f2b567c16e77b03182ee
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-05-31 20:26:56 -0600

    bcryptAdapter added

---
+++ src/common/adapters/bcrypt.adapter.ts
@@ -0,0 +1,15 @@
+import { compareSync, genSaltSync, hashSync } from 'bcrypt'
+
+export class BcryptAdapter {
+
+  public hash ( password: string ):string {
+    const salt = genSaltSync()
+    
+    return hashSync( password, salt )
+  }
+
+  public compare( password: string, hashed: string ) {
+    return compareSync( password, hashed )
+  }
+
+}
