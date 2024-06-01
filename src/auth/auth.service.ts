8f89c7dda9f1979a1c37934f926f06e12052d1d2
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-05-31 20:28:47 -0600

    AuthModule Created

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,106 @@
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
+
+import { User } from './entities/user.entity';
+import { CreateUserDto, LoginUserDto } from './dto';
+
+import { BcryptAdapter } from '../common/adapters/bcrypt.adapter'
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+
+  private readonly logger = new Logger('AuthService')
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+
+    private readonly jwtService: JwtService,
+    private readonly bcrypt: BcryptAdapter,
+  ) {}
+  
+  async create( createUserDto: CreateUserDto ) {
+
+    try {
+      
+      const { password, ...userData } = createUserDto
+      const user = this.userRepository.create({
+        ...userData,
+        password: this.bcrypt.hash( password )
+      })
+
+      await this.userRepository.save( user )
+      delete user.password
+
+      return {
+        ...user,
+        token: this.getJwtToken({ id: user.id })
+      }
+      // TODO: Retornar JWT
+
+      // return {
+      //   user: userData
+      // }
+    } catch (error) {
+
+      this.handleDBErrors( error )
+      
+    }
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+
+    const { email, password } = loginUserDto
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true, id: true }
+    })
+
+    if ( !user )
+      throw new UnauthorizedException('Credentials are not valid(email)')
+
+    if ( !this.bcrypt.compare( password, user.password ) )
+      throw new UnauthorizedException('Credentials are not valid(pass)')
+
+    console.log({ user })
+
+    return {
+      ...user,
+      token: this.getJwtToken({ id: user.id })
+    }
+    // Todo: Retornar el JWT 
+    // try {
+    // } catch (error) {
+    //   this.handleDBErrors(error)
+    // }
+
+  }
+
+  private getJwtToken( payload: JwtPayload ) {
+
+    const token = this.jwtService.sign( payload )
+
+    return token
+  }
+
+  private handleDBErrors( error: any ): never {
+    
+    this.logger.error(error)
+
+    if ( error.code === '23505' ) {
+      throw new BadRequestException( error.detail )
+    }
+    
+    if ( error.code === '23502' ){
+      throw new BadRequestException( error.detail )
+    }
+
+    throw new InternalServerErrorException('Please check server logs')
+
+  }
+
+}
